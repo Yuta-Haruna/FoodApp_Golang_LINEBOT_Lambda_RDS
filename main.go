32f7027705f9b41a612e48df860e2825fb09f611
Yuta-Haruna <50592688+Yuta-Haruna@users.noreply.github.com>
2023-04-02 20:58:19 +0900

    RDS接続準備中

--- main.go
+++ main.go
@@ -1,18 +1,24 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/line/line-bot-sdk-go/linebot"
 )
 
 // API Gatewayから受け取ったevents.APIGatewayProxyRequestのBody（JSON）をパースする
 // https://app.quicktype.io/　に、実際に受け取ってたJSONメッセージを張り付けて、コード自動生成。
+
+// 接続先DB情報
+var db *sql.DB
 
 // ▼▼▼ https://app.quicktype.io/で自動生成したコード：ここから ▼▼▼
 func UnmarshalLineRequest(data []byte) (LineRequest, error) {
@@ -72,17 +78,40 @@ func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyRespo
 		os.Getenv("CHANNEL_SECRET"),
 		os.Getenv("CHANNEL_ACCESS_TOKEN"),
 	)
+	fmt.Println("*** linebotConnect")
+
 	if err != nil {
-		log.Fatal(err)
+		fmt.Println(err)
+	}
+
+	// DBから取得した値を格納する
+	var resultCal *int
+	fmt.Println(myLineRequest.Events[0].Message.Text)
+
+	// DBから食品情報を取得する
+	dbErr := db.QueryRow("SELECT calQuantityKcal FROM foodAppDB.foodCalorieInformation WHERE Name LIKE ?", myLineRequest.Events[0].Message.Text).Scan(&resultCal)
+	fmt.Println("*** dbErr")
+	if dbErr != nil {
+		fmt.Println("データベース接続失敗")
+		panic(err.Error())
+	} else {
+		fmt.Println("データベース接続成功")
 	}
 
 	// リプライ実施
 	fmt.Println("*** reply")
 	var tmpReplyMessage string
-	tmpReplyMessage = "🦜オウムさん：" + myLineRequest.Events[0].Message.Text
+	if resultCal == nil {
+		tmpReplyMessage = "🦜オウムさん：" + myLineRequest.Events[0].Message.Text + "、そいつのカロリーは不明だ。食い物だったら登録してくれ😎"
+	} else {
+		tmpReplyMessage = "🦜オウムさん：" + myLineRequest.Events[0].Message.Text + "、そいつは" + strconv.Itoa(*resultCal) + "kcalだ!!"
+	}
 	if _, err = bot.ReplyMessage(myLineRequest.Events[0].ReplyToken, linebot.NewTextMessage(tmpReplyMessage)).Do(); err != nil {
 		log.Fatal(err)
 	}
+
+	// データベースの接続を閉じる
+	db.Close()
 
 	// 終了
 	fmt.Println("*** end")
@@ -91,4 +120,42 @@ func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyRespo
 
 func main() {
 	lambda.Start(Handler)
+}
+
+func init() {
+	// データベースの接続情報
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	// dbName := os.Getenv("DB_NAME")
+	// dbName := os.Getenv("foodAppDB")
+
+	// データベースに接続するための文字列
+	dataSourceName := fmt.Sprintf("%s:%s@tcp(%s:%s)/foodAppDB", user, password, host, port)
+	fmt.Println("*** dataSourceName : " + dataSourceName)
+	fmt.Println("*** DB")
+	// データベースに接続する
+	var err error
+	db, err = sql.Open("mysql", dataSourceName)
+
+	fmt.Println(db)
+	if err != nil {
+		fmt.Println("error -- ")
+		fmt.Println(err)
+		fmt.Println("error -- ")
+	}
+	fmt.Println("*** DB : Start")
+
+	// データベースの接続を確認する
+	dbErr := db.Ping()
+	if dbErr != nil {
+		fmt.Println("dbErr -- ")
+		fmt.Println(dbErr)
+		fmt.Println("dbErr -- ")
+
+	}
+	fmt.Println("Connected!")
+	fmt.Println("*** DB END")
+
 }
