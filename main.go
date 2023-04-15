8042bdc9355bd949d919c24b4daa761b3f67fcc9
Yuta-Haruna <50592688+Yuta-Haruna@users.noreply.github.com>
2023-04-15 22:38:41 +0900

    update: RDS接続&LINE返却完了　デバッグログ付き

--- main.go
+++ main.go
@@ -14,19 +14,17 @@ import (
 	"github.com/line/line-bot-sdk-go/linebot"
 )
 
-// API Gatewayから受け取ったevents.APIGatewayProxyRequestのBody（JSON）をパースする
-// https://app.quicktype.io/　に、実際に受け取ってたJSONメッセージを張り付けて、コード自動生成。
-
 // 接続先DB情報
 var db *sql.DB
 
-// ▼▼▼ https://app.quicktype.io/で自動生成したコード：ここから ▼▼▼
+// 取得したLINEの入力内容JSONを整形
 func UnmarshalLineRequest(data []byte) (LineRequest, error) {
 	var r LineRequest
 	err := json.Unmarshal(data, &r)
 	return r, err
 }
 
+// JSON構造体
 func (r *LineRequest) Marshal() ([]byte, error) {
 	return json.Marshal(r)
 }
@@ -55,10 +53,6 @@ type Source struct {
 	Type   string `json:"type"`
 }
 
-// ▲▲▲ https://app.quicktype.io/で自動生成したコード：ここまで ▲▲▲
-
-// Handler
-// fmt.Printlnやlog.Fatalは、CloudWatchのログで確認可能
 func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 
 	// 受け取ったJSONメッセージをログに書き込む（デバッグ用）
@@ -84,71 +78,40 @@ func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyRespo
 		fmt.Println(err)
 	}
 
-	// DBから取得した値を格納する
-	var resultCal *int
 	fmt.Println(myLineRequest.Events[0].Message.Text)
 
-	// DBから食品情報を取得する
-	dbErr := db.QueryRow("SELECT calQuantityKcal FROM foodAppDB.foodCalorieInformation WHERE Name LIKE ?", myLineRequest.Events[0].Message.Text).Scan(&resultCal)
-	fmt.Println("*** dbErr")
-	if dbErr != nil {
-		fmt.Println("データベース接続失敗")
-		panic(err.Error())
-	} else {
-		fmt.Println("データベース接続成功")
-	}
-
-	// リプライ実施
-	fmt.Println("*** reply")
-	var tmpReplyMessage string
-	if resultCal == nil {
-		tmpReplyMessage = "🦜オウムさん：" + myLineRequest.Events[0].Message.Text + "、そいつのカロリーは不明だ。食い物だったら登録してくれ😎"
-	} else {
-		tmpReplyMessage = "🦜オウムさん：" + myLineRequest.Events[0].Message.Text + "、そいつは" + strconv.Itoa(*resultCal) + "kcalだ!!"
-	}
-	if _, err = bot.ReplyMessage(myLineRequest.Events[0].ReplyToken, linebot.NewTextMessage(tmpReplyMessage)).Do(); err != nil {
-		log.Fatal(err)
-	}
-
-	// データベースの接続を閉じる
-	db.Close()
-
-	// 終了
-	fmt.Println("*** end")
-	return events.APIGatewayProxyResponse{Body: request.Body, StatusCode: 200}, nil
-}
-
-func main() {
-	lambda.Start(Handler)
-}
-
-func init() {
 	// データベースの接続情報
 	host := os.Getenv("DB_HOST")
 	port := os.Getenv("DB_PORT")
 	user := os.Getenv("DB_USER")
 	password := os.Getenv("DB_PASSWORD")
-	// dbName := os.Getenv("DB_NAME")
-	// dbName := os.Getenv("foodAppDB")
+	dbName := os.Getenv("DB_NAME")
 
 	// データベースに接続するための文字列
-	dataSourceName := fmt.Sprintf("%s:%s@tcp(%s:%s)/foodAppDB", user, password, host, port)
+	dataSourceName := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", user, password, host, port, dbName)
 	fmt.Println("*** dataSourceName : " + dataSourceName)
 	fmt.Println("*** DB")
 	// データベースに接続する
-	var err error
-	db, err = sql.Open("mysql", dataSourceName)
+	var dbErr error
+	db, dbErr = sql.Open("mysql", dataSourceName)
+
+	// 処理の最後にデータベースの接続を閉じる
+	defer func() {
+		if cerr := db.Close(); cerr != nil {
+			log.Printf("Failed to close database: %v", cerr)
+		}
+	}()
 
 	fmt.Println(db)
-	if err != nil {
+	if dbErr != nil {
 		fmt.Println("error -- ")
-		fmt.Println(err)
+		fmt.Println(dbErr)
 		fmt.Println("error -- ")
 	}
 	fmt.Println("*** DB : Start")
 
 	// データベースの接続を確認する
-	dbErr := db.Ping()
+	dbErr = db.Ping()
 	if dbErr != nil {
 		fmt.Println("dbErr -- ")
 		fmt.Println(dbErr)
@@ -158,4 +121,58 @@ func init() {
 	fmt.Println("Connected!")
 	fmt.Println("*** DB END")
 
+	// 最終表記
+	var tmpReplyMessage string
+
+	// DBから食品情報を取得する
+	query := "SELECT calQuantityKcal FROM foodCalorieInformation WHERE Name LIKE ? LIMIT 1"
+	rows, dbErr := db.Query(query, myLineRequest.Events[0].Message.Text)
+
+	fmt.Println("*** dbErr")
+	if dbErr != nil {
+		fmt.Println("データベース接続失敗")
+		fmt.Println(dbErr)
+	}
+	// 処理終了後 SQLの停止
+	defer rows.Close()
+
+	// クエリの結果を処理する
+	if !rows.Next() {
+		// クエリの結果が空だった場合の処理
+		fmt.Println("あああああ")
+		tmpReplyMessage = "🦜オウムさん：" + myLineRequest.Events[0].Message.Text + "、そいつのカロリーは不明だ。食い物だったら登録してくれ😎"
+	} else {
+		// クエリの結果があった場合の処理
+		// var column1 string
+		var column1 int
+		if err := rows.Scan(&column1); err != nil {
+			fmt.Println("データベース接続成功 : クエリ失敗")
+			fmt.Println(err)
+		}
+		fmt.Printf("column1: %d", column1)
+		tmpReplyMessage = "🦜オウムさん：" + myLineRequest.Events[0].Message.Text + "、そいつは" + strconv.Itoa(column1) + "kcalだ!!"
+
+	}
+
+	// リプライ実施
+	fmt.Println("*** reply")
+	fmt.Println(tmpReplyMessage)
+
+	fmt.Println(myLineRequest.Events[0].ReplyToken)
+
+	// LINEBOTへの連絡内容を設定
+	replyMessage, err := bot.ReplyMessage(myLineRequest.Events[0].ReplyToken, linebot.NewTextMessage(tmpReplyMessage)).Do()
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(replyMessage)
+	}
+
+	// 終了
+	fmt.Println("*** end")
+	return events.APIGatewayProxyResponse{Body: request.Body, StatusCode: 200}, nil
+}
+
+func main() {
+	lambda.Start(Handler)
 }
